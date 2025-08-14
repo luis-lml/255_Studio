1e19cfd1ab9fc6e913c526c0ceae2d1fd18e533f
luis-lml <l.mrmlj@gmail.com>
2025-08-13 20:50:01 -0600

    cambios en la seguridad

--- src/main/java/com/photobooking_api/model/Client.java
+++ src/main/java/com/photobooking_api/model/Client.java
@@ -6,6 +6,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -19,9 +21,15 @@ public class Client {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "El nombre del cliente no puede estar vacío.")
     @Column(nullable = false)
     private String name;
 
+    @NotBlank(message = "El apellido del cliente no puede estar vacío.")
+    @Column(nullable = false)
+    private String lastname;
+
+    @Email(message = "El formato del email no es válido.")
     @Column(unique = true) // Opcional: hacer que el email sea único
     private String email;
 }
